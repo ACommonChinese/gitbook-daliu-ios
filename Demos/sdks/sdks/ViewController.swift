0cbd069819e99bdd013ebf91f2558923661fb1bc
ACommonChinese <liuxing8807@126.com>
2020-05-05 13:45:13 +0800

    add config

---
+++ Demos/sdks/sdks/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  sdks
+//
+//  Created by liuweizhen on 2020/5/5.
+//  Copyright © 2020 liuxing8807@126.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+
+    func setup() {
+        self.title = "sdks"
+        
+    }
+}
+
